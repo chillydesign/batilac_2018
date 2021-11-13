c68d85004289a5e893f226d7c8bce60623e6444f
Charles Harvey <harvey.charles@gmail.com>
2021-11-13 10:47:32 +0000

    add if statement for gallery on single promotion

--- single-promotion.php
+++ single-promotion.php
@@ -1,40 +1,45 @@
 <?php get_header(); ?>
 <div class="concrete">
-		<div class="container">
-			<h1><?php the_title(); ?></h1>
-		</div>
+	<div class="container">
+		<h1><?php the_title(); ?></h1>
 	</div>
+</div>
 
 <section class="container">
 
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<?php $gallery = get_field('gallery'); ?>
-
-	<div id="slider_realisations">
-		<ul class="bxslider">
-			<?php foreach( $gallery as $image ): ?>
-				<li><div class="slider_contain" style="background-image:url(<?php echo $image['url']; ?>); background-color:#efefef; padding:20%; margin-bottom:30px;"></div></li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-
-	<div>
-		<h3>Description</h3>
-	<?php the_content(); // Dynamic Content ?>
-			<?php $file = get_field('documentation'); ?>
-			<?php if ($file) : ?>
-				<p><a target="_blank" href="<?php echo $file['url']; ?>" class="file_download">Documentation</a></p>
+			<?php $gallery = get_field('gallery'); ?>
+			<?php if ($gallery) : ?>
+				<div id="slider_realisations">
+					<ul class="bxslider">
+						<?php foreach ($gallery as $image) : ?>
+							<li>
+								<div class="slider_contain" style="background-image:url(<?php echo $image['url']; ?>); background-color:#efefef; padding:20%; margin-bottom:30px;"></div>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
 			<?php endif; ?>
-	</div>
+
+			<div>
+				<h3>Description</h3>
+				<?php the_content(); // Dynamic Content 
+				?>
+				<?php $file = get_field('documentation'); ?>
+				<?php if ($file) : ?>
+					<p><a target="_blank" href="<?php echo $file['url']; ?>" class="file_download">Documentation</a></p>
+				<?php endif; ?>
+			</div>
 
 </section>
 
-	<!-- <div class="row">
+<!-- <div class="row">
 		<div class="col-sm-6 col-sm-push-6">
 
-				<?php the_content(); // Dynamic Content ?>
+				<?php the_content(); // Dynamic Content 
+				?>
 				<?php $file = get_field('documentation'); ?>
 				<?php if ($file) : ?>
 					<p><a target="_blank" href="<?php echo $file['url']; ?>" class="file_download">Documentation</a></p>
@@ -45,7 +50,7 @@
 
 				<div id="slider_realisations">
 					<ul class="bxslider">
-						<?php foreach( $gallery as $image ): ?>
+						<?php foreach ($gallery as $image) : ?>
 							<li><div class="slider_contain" style="background-image:url(<?php echo $image['url']; ?>); "></div></li>
 						<?php endforeach; ?>
 					</ul>
@@ -56,12 +61,12 @@
 
 <?php endwhile; ?>
 
-<?php else: ?>
+<?php else : ?>
 
 
 	<article>
 
-		<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+		<h1><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h1>
 
 	</article>
 	<!-- /article -->
